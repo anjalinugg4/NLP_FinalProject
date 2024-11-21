22ace3f56dc4ceee71c3183fe581c30772343fd2
angiezh <angiezhou2@gmail.com>
2024-11-20 20:33:54 -0800

    tried adding sentiment analysis for spacy text blob but didn't work

--- final project/spacey_test.py
+++ final project/spacey_test.py
@@ -1,10 +1,17 @@
 import spacy
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+from spacytextblob.spacytextblob import SpacyTextBlob
 
 # Load the English model
 nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe("spacytextblob")
+
 
 # Process a sentence
-doc = nlp(". @ RichLowry is truly one of the dumbest of the talking heads - he doesn't have a clue!")
+text = ". @ RichLowry is truly one of the dumbest of the talking heads - he doesn't have a clue!"
+doc = nlp(text)
+blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
 
 # Print tokenized words and their sentiment (using built-in attributes)
 print("Tokens and their parts of speech:")
@@ -15,3 +22,13 @@ for token in doc:
 print("\nNamed Entities:")
 for ent in doc.ents:
     print(f"{ent.text} -> {ent.label_}")
+
+
+# Sentiment analysis
+# isn't working right now
+print(f"Polarity (NaiveBayes): {blob.sentiment.p_pos - blob.sentiment.p_neg}") 
+print(f"Subjectivity: {blob.sentiment}")
+
+print("\nDetailed Sentiment Assessments:")
+for assessment in doc._.blob.sentiment_assessments.assessments:
+    print(assessment)
